ed5479fb6d7c9de3b5e0dc7d8e62cba038d93f89
John Liter <62905216+johnliter@users.noreply.github.com>
2024-12-17 05:12:56 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,52 +1,13 @@
 //Create Web Server
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var comments = [];
-var server = http.createServer(function (req, res) {
-    //parse url
-    var parseObj = url.parse(req.url, true);
-    var pathName = parseObj.pathname;
-    if (pathName === '/') {
-        fs.readFile('./views/index.html', function (err, data) {
-            if (err) {
-                return res.end('404 Not Found');
-            }
-            res.end(data);
-        });
-    } else if (pathName === '/post') {
-        fs.readFile('./views/post.html', function (err, data) {
-            if (err) {
-                return res.end('404 Not Found');
-            }
-            res.end(data);
-        });
-    } else if (pathName.indexOf('/public/') === 0) {
-        fs.readFile('.' + pathName, function (err, data) {
-            if (err) {
-                return res.end('404 Not Found');
-            }
-            res.end(data);
-        });
-    } else if (pathName === '/comments') {
-        var data = JSON.stringify(comments);
-        res.end(data);
-    } else if (pathName === '/comment') {
-        var comment = parseObj.query;
-        comment.dateTime = '2018-05-12 12:12:12';
-        comments.push(comment);
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        res.end();
-    } else {
-        fs.readFile('./views/404.html', function (err, data) {
-            if (err) {
-                return res.end('404 Not Found');
-            }
-            res.end(data);
-        });
-    }
+const express = require('express');
+const app = express();
+
+//Create a route
+app.get('/', (req, res) => {
+    res.send('Hello World');
 });
-server.listen(3000, function () {
-    console.log('Server is running...');
+
+//Listen on port 3000
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
 });
