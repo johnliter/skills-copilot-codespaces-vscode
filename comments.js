3e716a4ba92d4542c0f627e92e89242d5b7b8f17
John Liter <62905216+johnliter@users.noreply.github.com>
2024-12-17 05:15:08 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,2 @@
 //Create Web Server
-const express = require('express');
-const app = express();
-
-//Create a route
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
-//Listen on port 3000
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
+var http = require('http');
